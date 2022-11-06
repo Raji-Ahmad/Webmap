1c2606cf031abd2acb570b3de4db7f4da999de57
unknown <rajikolex99@gmail.com>
2022-11-06 11:22:03 +0100

    Adeed greenwood

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 var map = L.map('map').setView([29.7341051, -95.3819074], 19);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxNativeZoom:22,
     maxZoom:25
+})
+
+var google =   L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 22,
+    subdomains:['mt0']
+});
+
+googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 22,
+    subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
+
+var baseLayers = {
+    "OpenStreetMap": osm,
+    "Google": google,
+    "Satellite": googleSat
+};
 
 // function selectBuilding(bulding){
      
@@ -12,22 +28,61 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // $.getJSON('houston2.geojson', function (data) {
 //     // Define the geojson layer and add it to the map
-//     //L.geoJson(data).addTo(map);
+//     //L.Proj.geoJson(data).addTo(map);
 // });
 
+function loadGreenwood(){
 
-var l1 = $.getJSON('jsons/Level_1.json')
-var l2 = $.getJSON('jsons/Level_2.json')
-var l3 = $.getJSON('jsons/Level_3.json')
-var l11 = $.getJSON('jsons/Level_11-14.json')
+var floor1 = L.Proj.geoJson(l1)
+var floor2 = L.Proj.geoJson(l2)
+var floor3 = L.Proj.geoJson(l3)
+var floor11 = L.Proj.geoJson(l11)
+
+var floor1p = L.Proj.geoJson(l1p)
+var floor2p = L.Proj.geoJson(l2p)
+var floor3p = L.Proj.geoJson(l3p)
+var floor11p = L.Proj.geoJson(l11p)
+
+var lineGroup = L.featureGroup([floor1, floor2, floor3, floor11]);
+var polygonGroup = L.featureGroup([floor1p, floor2p, floor3p, floor11p]);
+
+var greenwood = L.featureGroup([lineGroup, polygonGroup]).addTo(map);
+//map.fitBounds(greenwood.getBounds());
+var center = greenwood.getBounds().getCenter();
+map.setView(center, 19)
 
 
-var l1p = $.getJSON('jsons/Level_1P.json')
-var l2p = $.getJSON('jsons/Level_2P.json')
-var l3p = $.getJSON('jsons/Level_3P.json')
-var l11p = $.getJSON('jsons/Level_11-14P.json')
+var overlays = {
+    "Greenwood": greenwood,
+
+    "Building Lines": polygonGroup,
+    "Plan": lineGroup,
+
+    "floor 1": floor1,
+    "floor 2": floor2, 
+    "floor 3 - 10": floor3, 
+    "floor 11 - 14": floor11,
+
+    "floor 1p": floor1p,
+    "floor 2p": floor2p, 
+    "floor 3 - 10p": floor3p, 
+    "floor 11 - 14p": floor11p
+    
+};
+
+L.control.layers(baseLayers, overlays).addTo(map);
+
+return greenwood;
+
+}
+
+var greenwood = loadGreenwood();
+var plan = L.Proj.geoJson(data, {maxZoom:25});
+// plan.addTo(map);
+// // var legend = L.control({position: 'bottomright'});
+// // legend.addTo(map);
 
 
-L.geoJson(l1).addTo(map)
-// var plan = L.geoJSON(data, {maxZoom:25}).addTo(map);
+
+
 L.control.scale().addTo(map);
